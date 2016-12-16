22fbaf4274ffa6f09b6f3cc781f84d54065ae20f
Sherry_khan <sheryar.khan@goldevelopers.com>
2016-12-16 10:38:32 +0500

    delete api change

--- delete.php
+++ delete.php
@@ -2,8 +2,12 @@
 $servername="us-cdbr-iron-east-04.cleardb.net";
 $username="b77cfb6e5654c6";
 $password="db79ff493032d7a";
+//$servername="localhost";
+//$username="root";
+//$password="";
 
-$con= mysqli_connect($servername,$user_name,$password);
+$con= mysqli_connect($servername,$username,$password);
+		//mysqli_select_db( $con ,"Webservice");
 		mysqli_select_db( $con ,"heroku_7bf75d7fb8c2e8a");
 		if($con->connect_error)
 		{
